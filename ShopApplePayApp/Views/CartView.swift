3181c0f3f4db941833d8922ed452ed0ee489c673
luca.berardinelli@iquii.com <luca.berardinelli@iquii.com>
2022-08-25 21:51:11 +0200

    apple pay button

--- ShopApplePayApp/Views/CartView.swift
+++ ShopApplePayApp/Views/CartView.swift
@@ -12,19 +12,27 @@ struct CartView: View {
     
     var body: some View {
         ScrollView {
-            if cartManager.products.count > 0 {
-                ForEach(cartManager.products, id: \.id) { product in
-                    ProductRow(product: product)
-                }
-                HStack {
-                    Text("Your cart total is")
-                    Spacer()
-                    Text("$\(cartManager.total).00")
-                        .bold()
-                }
-                .padding()
+            if cartManager.paymentSuccess {
+                Text("Thanks for your purchase! You'll get cozy in our comfy sweaters soon! You'll also receive an email confirmation shortly.")
+                    .padding()
             } else {
-                Text("Your cart is empty")
+                if cartManager.products.count > 0 {
+                    ForEach(cartManager.products, id: \.id) { product in
+                        ProductRow(product: product)
+                    }
+                    HStack {
+                        Text("Your cart total is")
+                        Spacer()
+                        Text("$\(cartManager.total).00")
+                            .bold()
+                    }
+                    .padding()
+                    
+                    PaymentButton(action: cartManager.pay)
+                        .padding()
+                } else {
+                    Text("Your cart is empty")
+                }
             }
         }
         .navigationTitle(Text("My cart"))
